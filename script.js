685884dec305fd7350e0202f0456f1e1241c2a5e
jbrunops <jbrunops@outlook.com>
2024-12-23 21:19:08 -0300

    Criando um objeto com os detalhes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+const form = document.getElementById("form");
 
 // Input só aceitando números agora!
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
+
+  // Transformar o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$1)
+  value = Number(value) / 100;
+
   amount.value = formatCurrencyBRL(value);
 };
 
@@ -14,5 +21,19 @@ function formatCurrencyBRL(value) {
     style: "currency",
     currency: "BRL",
   });
+  // Retorna o valor formatado
   return value;
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+};
