d5c94726602e25012adf0b1122f439b0d04feb91
jbrunops <jbrunops@outlook.com>
2024-12-23 21:04:16 -0300

    Formatando o valor no padrão BRL

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ const amount = document.getElementById("amount");
 // Input só aceitando números agora!
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
-  amount.value = value;
+  amount.value = formatCurrencyBRL(value);
 };
+
+// Função para formatar o valor
+function formatCurrencyBRL(value) {
+  // Formatando o valor no padrão BRL
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
