8e920be99b22d154e03322882d005ba96b5f3cb7
jbrunops <jbrunops@outlook.com>
2024-12-23 21:00:50 -0300

    Javascript add

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("teste");
+// Seleciona os elementos do formulário
+const amount = document.getElementById("amount");
+
+// Input só aceitando números agora!
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+  amount.value = value;
+};
